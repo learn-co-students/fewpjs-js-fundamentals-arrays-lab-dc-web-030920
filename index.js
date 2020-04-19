2e79c45108cad604f99c799db809eda39527f5d4
Dawit Gizaw <gizawdawit123.com>
2020-04-19 19:36:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution here!
+var pets = ["Milo", "Otis", "Garfield"];
+
+const append = [...pets, "Odie"];
+const prepend = ["Odie", ...pets];
+const removeLast = pets.slice(0, pets.length - 1);
+const removeFirst = pets.slice(1);
+
